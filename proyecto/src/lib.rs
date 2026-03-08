bfa93b153bba9a6f842dbbb49e5ec54f4c5ee9e1
Omar Ornelas <ornelashernandezomareduardo@gmail.com>
2026-03-08 14:18:40 -0600

    update lib.rs

--- proyecto/src/lib.rs
+++ proyecto/src/lib.rs
@@ -1,18 +1,79 @@
 use anchor_lang::prelude::*;
 
-declare_id!("");
+declare_id!("GQvz3kmWK9wKu37C3vnnY36cEgPTrpaN5CyeVSUgemwJ");
 
 #[program]
-mod modulo {
+pub mod las_machas_cloud {
     use super::*;
 
-    pub fn mi_primer_instruccion(ctx: Context<Saludo>) -> Result<()> {
-        msg!("Mi instruccion, funciona !!! :D");
+    pub fn inicializar_inventario(ctx: Context<InicializarInventario>, nombre: String) -> Result<()> {
+        let inventario = &mut ctx.accounts.inventario;
+        inventario.owner = *ctx.accounts.owner.key;
+        inventario.nombre = nombre;
+        Ok(())
+    }
+
+    pub fn agregar_salsa(ctx: Context<AgregarSalsa>, nombre: String, nivel_picor: String, cantidad: u32) -> Result<()> {
+        let inventario = &mut ctx.accounts.inventario;
+        
+        let nueva_salsa = Salsa {
+            nombre,
+            nivel_picor,
+            cantidad,
+        };
+        
+        inventario.salsas.push(nueva_salsa);
+        Ok(())
+    }
+
+    pub fn ver_inventario(ctx: Context<VerInventario>) -> Result<()> {
+        let inventario = &ctx.accounts.inventario;
+        msg!("Inventario de: {}", inventario.nombre);
+        for salsa in &inventario.salsas {
+            msg!("Salsa: {}, Picor: {}, Stock: {}", salsa.nombre, salsa.nivel_picor, salsa.cantidad);
+        }
         Ok(())
     }
 }
 
+#[derive(Accounts)]
+#[instruction(nombre: String)]
+pub struct InicializarInventario<'info> {
+    #[account(
+        init,
+        payer = owner,
+        space = 8 + 32 + 50 + 4 + (10 * 100), 
+        seeds = [b"inventario", owner.key().as_ref()],
+        bump
+    )]
+    pub inventario: Account<'info, Inventario>,
+    #[account(mut)]
+    pub owner: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
 
 #[derive(Accounts)]
-pub struct Saludo {}
+pub struct AgregarSalsa<'info> {
+    #[account(mut, has_one = owner)]
+    pub inventario: Account<'info, Inventario>,
+    pub owner: Signer<'info>,
+}
 
+#[derive(Accounts)]
+pub struct VerInventario<'info> {
+    pub inventario: Account<'info, Inventario>,
+}
+
+#[account]
+pub struct Inventario {
+    pub owner: Pubkey,
+    pub nombre: String,
+    pub salsas: Vec<Salsa>,
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize, Clone)]
+pub struct Salsa {
+    pub nombre: String,
+    pub nivel_picor: String,
+    pub cantidad: u32,
+}
